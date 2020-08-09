7bee039532e4b65d3b60c349a0f1c9c3fae41063
Lorenzo Gandino <lorenzoga94@gmail.com>
2020-08-09 02:05:14 +0200

    rimosso alias lt siccome inutile fare un tail con 0

--- tools.sh
+++ tools.sh
@@ -7,7 +7,6 @@ function alias-container-actions() {
 	alias ${prefix}status="docker inspect --format '{{ .State.Health.Status }}' ${container}"
 	alias ${prefix}l="docker logs ${container}"
 	alias ${prefix}lf="docker logs -f ${container}"
-	alias ${prefix}lt="docker logs --tail=0 ${container}"
 	alias ${prefix}ltf="docker logs --tail=0 -f ${container}"
 	alias ${prefix}bash="winpty docker exec -it ${container} bash"
 	alias ${prefix}e="docker exec ${container}"
