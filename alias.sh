ca9f953adf32405abc2b033da9eed4b888f7546e
Lorenzo Gandino <lorenzoga94@gmail.com>
2020-08-11 18:38:01 +0200

    add histg='history | grep'

--- alias.sh
+++ alias.sh
@@ -38,6 +38,7 @@ alias removecolor='sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g"'
 alias loadalias='source ~/.alias.sh'
 alias jsonmini='jq -c .'
 alias jsonformat='jq .'
+alias histg='history | grep'
 
 # Docker
 
