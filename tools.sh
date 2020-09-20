102670b49e8b04668bedea94635aaba14b179c4a
Lorenzo Gandino <lorenzoga94@gmail.com>
2020-09-20 22:12:41 +0200

    Removed command to suppoty MINGW64

--- tools.sh
+++ tools.sh
@@ -9,23 +9,23 @@ function alias-container-actions() {
 	alias ${prefix}l="docker logs ${container}"
 	alias ${prefix}lf="docker logs -f ${container}"
 	alias ${prefix}ltf="docker logs --tail=0 -f ${container}"
-	alias ${prefix}bash="winpty docker exec -it ${container} bash"
-	alias ${prefix}sh="winpty docker exec -it ${container} sh"
+	alias ${prefix}bash="docker exec -it ${container} bash"
+	alias ${prefix}sh="docker exec -it ${container} sh"
 	alias ${prefix}e="docker exec ${container}"
-	alias ${prefix}ei="winpty docker exec -it ${container}"
+	alias ${prefix}ei="docker exec -it ${container}"
 }
 
 function run-docker-command() {
 	command_container=$1
 	command_run=$2
-	bash -c "export MSYS_NO_PATHCONV=1; winpty bash -c \"docker run --rm -it -v \\\"$(cygpath -w $PWD):/app\\\" -w /app '$command_container' '$command_run'\""
+	bash -c "bash -c \"docker run --rm -it -v \\\"$PWD:/app\\\" -w /app '$command_container' '$command_run'\""
 }
 
 function alias-docker-shell-command() {
 	command_name=$1
 	command_container=$2
 	command_run=$3
-	alias d${command_name}='bash -c "export MSYS_NO_PATHCONV=1; winpty bash -c \"docker run --rm -it -v \\\"$(cygpath -w $PWD):/app\\\" -w /app '$command_container' '$command_run'\""'
+	alias d${command_name}='bash -c "bash -c \"docker run --rm -it -v \\\"$PWD:/app\\\" -w /app '$command_container' '$command_run'\""'
 }
 
 function alias-ssh-server() {
